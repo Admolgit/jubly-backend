f54d78bf73a9d8b329a932243829cd1c4368dfb0
Admolpeerless <ismaila@bepeerless.co>
2026-04-06 12:02:20 +0100

    feat: added clients stats and other features

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -45,6 +45,8 @@ export class TransactionService {
           senderDetailsId: dto.senderDetailsId,
           currency: 'NGN',
           paidAt: new Date(),
+          status: dto.status,
+          percentageFee: dto.percentageFee,
           providerRef: dto.providerRef,
         },
       });
@@ -58,9 +60,9 @@ export class TransactionService {
 
   async findAllVendorTransactions(
     vendorId: string,
-    page: number,
-    limit: number,
-    search: string,
+    page?: number,
+    limit?: number,
+    search?: string,
   ) {
     try {
       let where: any = {};
@@ -77,8 +79,8 @@ export class TransactionService {
 
       const transactions = await this.prisma.transaction.findMany({
         where,
-        skip: (page - 1) * limit,
-        take: Number(limit),
+        skip: page && limit ? (Number(page) - 1) * Number(limit) : undefined,
+        take: limit ? Number(limit) : undefined,
         orderBy: { createdAt: 'desc' },
         include: {
           senderDetails: {
