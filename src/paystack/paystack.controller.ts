110aa903d25062649d6a904b41b25f329572cd7f
Admolpeerless <ismaila@bepeerless.co>
2026-08-29 22:36:45 +0100

    feat: subscription flow added

--- src/paystack/paystack.controller.ts
+++ src/paystack/paystack.controller.ts
@@ -477,7 +477,9 @@ export class PaystackController {
           !startTime ||
           !endTime
         ) {
-          throw new BadRequestException('Incomplete booking metadata in marketplace');
+          throw new BadRequestException(
+            'Incomplete booking metadata in marketplace',
+          );
         }
 
         const book = await this.bookingService.createBooking(vendorUserId, {
